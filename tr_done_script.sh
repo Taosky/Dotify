d37f9c488c4af78c5b065fe96dda9031749e6125
Taosky <t@firefoxcn.net>
2023-02-12 18:16:44 +0800

    🎈 perf(tr_done_script): 简化脚本,提高适用性

--- tr_done_script.sh
+++ tr_done_script.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-PARAM=$(echo $TR_TORRENT_DIR | tr -d '\n' | xxd -plain | sed 's/\(..\)/%\1/g' | tr -d "\n")
+function urlencode() {
+  which "curl" >/dev/null 2>&1; if [ ! $? -eq 0 ]; then echo -E "$1";return; fi
+  encode_str=$(echo -E "$1" |sed "s/%/%%/g")
+  printf -- "$encode_str" | curl -Gso /dev/null -w %{url_effective} --data-urlencode @- "" |cut -c 3-
+}
+PARAM=$(urlencode "$TR_TORRENT_DIR")
 
-curl "http://127.0.0.1:4023/downloaded?dir=$PARAM" -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:74.0) Gecko/20100101 Firefox/74.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: zh-CN,en-US;q=0.7,en;q=0.3' --compressed -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'Cache-Control: max-age=0'
+curl "http://127.0.0.1:4023/downloaded?dir=$PARAM"
