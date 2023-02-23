85110660b5c8afbfd0c90f3902402c9013932bb2
Taosky <t@firefoxcn.net>
2023-02-23 22:42:16 +0800

    ✨ feat(script): 支持qBittorrent

---
+++ qb_done.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+SAVE_DIR=$1
+echo SAVE_DIR
+
+function urlencode() {
+  which "curl" >/dev/null 2>&1; if [ ! $? -eq 0 ]; then echo -E "$1";return; fi
+  encode_str=$(echo -E "$1" |sed "s/%/%%/g")
+  printf -- "$encode_str" | curl -Gso /dev/null -w %{url_effective} --data-urlencode @- "" |cut -c 3-
+}
+PARAM=$(urlencode "$SAVE_DIR/")
+
+curl "http://127.0.0.1:4023/downloaded?dir=$PARAM"
